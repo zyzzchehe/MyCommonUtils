1197b625d49b84429ad2db11c5aee648a64a0080
zhangyz19 <zhangyz19@lenovo.com>
2021-04-25 11:47:28 +0800

    20210425 更改MLog存储卡路径为动态，而不是写死2

--- app/src/main/java/com/zc/mycommonutils/MainActivity.java
+++ app/src/main/java/com/zc/mycommonutils/MainActivity.java
@@ -3,16 +3,31 @@ package com.zc.mycommonutils;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import com.zc.commonutilslib.AlertDialogUtil;
+import com.zc.commonutilslib.MLog;
+
+import permison.PermissonUtil;
+import permison.listener.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AlertDialogUtil.showDialogWithOk(this,"","你好","确定");
+        PermissonUtil.checkPermission(this, new PermissionListener() {
+            @Override
+            public void havePermission() {
+                MLog.i(TAG, "havePermission: ");
+            }
+
+            @Override
+            public void requestPermissionFail() {
+                MLog.i(TAG, "requestPermissionFail: ");
+            }
+        }, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
     }
 }
